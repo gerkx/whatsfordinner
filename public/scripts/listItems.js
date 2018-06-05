11baed803964670b0b883524bea489795244b094
Patrick Gerke <ptgerke@gmail.com>
2018-06-05 16:05:43 +0200

    refined date time picker

--- public/scripts/listItems.js
+++ public/scripts/listItems.js
@@ -2,37 +2,26 @@
 // date picker setup //
 ///////////////////////
 var today = new Date();
-var currentYear = today.getFullYear();
-
-var nextYear = new Date(today.getFullYear() + 1);
-var maxDate = new Date(today.getTime() + 28 * 24 * 60 * 60 * 1000);
-var dateStr = today.toString();
-var prevDate = new Date('2018-06-06');
-
-// var field = document.getElementById('datepicker');
-console.log(prevDate);
+var prevDate = new Date("Jun 07 2018");
 
 var field = document.getElementById("datepicker")
-
-// var boop = "";
-
-field.innerHTML = prevDate;
+field.innerHTML = prevDate.toDateString();
 
 var picker = new Pikaday({
     field: field,
     trigger: document.getElementById('datepicker-button'),
     firstDay: 1,
     minDate: today,
-    maxDate: maxDate,
-    yearRange: [currentYear, nextYear],
+    maxDate: new Date(today.getTime() + 28 * 24 * 60 * 60 * 1000),
+    yearRange: [
+        today.getFullYear(), 
+        new Date(today.getFullYear() + 1)
+    ],
     onSelect: function(date){
-        var boop = picker.toString();
-        console.log("boop: ", boop);
-        field.innerHTML = boop;
-
+        prevDate = picker;
+        console.log("boop: ", prevDate.toString());
+        field.innerHTML = prevDate;
     }
-
-
 });
 
 
