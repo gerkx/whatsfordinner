d1a183f7a392e757dd670492c9effd670f9d33b2
Patrick Gerke <ptgerke@gmail.com>
2018-08-12 14:16:43 +0200

    began implementing a db seed function for dev streamlining

---
+++ seed/seed.js
@@ -0,0 +1,32 @@
+const csv = require('csvtojson');
+const foodFile = './foodItemSeed.csv';
+const mongoose = require('mongoose');
+const db = require('../models');
+
+const seedDB = (model, file) => {
+    db[model].remove({}, err => {
+        if(err) console.log(err);
+        else 
+            console.log(`${model} wiped`);
+            csv().fromFile(file)
+                .then(jsonObj=>jsonObj.shuffle())
+                .then(arr => arr.forEach(
+                    item => {
+                        db[model].create(item), (err, food) =>{
+                            if(err) console.log(err);
+                            else console.log(`${food} saved!`);
+                        }
+                    }
+                ));
+    });
+}
+
+seedDB("FoodModel", foodFile);
+
+Array.prototype.shuffle = function(){
+    this.forEach( function(val, idx, arr){
+        const seed = Math.floor(Math.random() * (idx+1));
+        [arr[idx], arr[seed]] = [arr[seed], arr[idx]];
+    });
+    return this;
+}
