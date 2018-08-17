8279a751662bf9e567a3b1cbdd5832862c2d0f60
Patrick Gerke <ptgerke@gmail.com>
2018-08-18 01:05:15 +0200

    heroku fixing

--- models/index.js
+++ models/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 mongoose.set('debug', true);
 
-const dbURL = process.env.DATABASEURL || 'mongodb://gerkx:iloveamanda23@ds125352.mlab.com:25352/wfdbeta';
+const dbURL = process.env.MONGODB_URI || 'mongodb://heroku_mq4jwvvw:ego42fchb9pcq8j4ol187qbeu3@ds125372.mlab.com:25372/heroku_mq4jwvvw';
 mongoose.connect(dbURL, { useNewUrlParser: true })
 
 mongoose.Promise = Promise;
